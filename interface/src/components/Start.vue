161c5a2b3256b60e44bf1b84e693d6af81efd8d0
vinney cavallo <vcavallo@gmail.com>
2023-02-10 16:07:27 -0500

    general: move desk into urbit directory; create interface directory; add list.txt to gitignore

---
+++ interface/src/components/Start.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Hello world</h1>
+  This is Start.vue. Be sure to open this file and change <pre>const deskname</pre> to the name of your desk.
+  You'll find tailwind css at <span class="your-class-here">src/index.css</span>.
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, computed } from 'vue';
+import { useStore } from '@/store/store'
+import { ActionTypes } from '@/store/action-types';
+import { GetterTypes } from '@/store/getter-types';
+
+const store = useStore()
+
+onMounted(() => {
+  const deskname = 'urdl-user'
+  startAirlock(deskname)
+})
+
+onUnmounted(() => {
+  // Maybe:
+  // closeAirlock()
+})
+
+const computedThings = computed(() => store.state.exampleThings)
+const fromGetters = computed(() => {
+  return store.getters[GetterTypes.EXAMPLE_WITH_ARG]('arg here');
+})
+
+const startAirlock = (deskname: string) => {
+  store.dispatch(ActionTypes.AIRLOCK_OPEN, deskname)
+}
+
+const closeAirlock = () => {
+  // Maybe you want this.
+}
+</script>
+
+
