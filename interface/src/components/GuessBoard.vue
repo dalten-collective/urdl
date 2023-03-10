4947c4325d5739ecce0cea6eccd1a9bfc4bc1e58
vinney cavallo <vcavallo@gmail.com>
2023-03-10 16:25:01 -0500

    ui: update 'enter' and 'backspace' keys

--- interface/src/components/GuessBoard.vue
+++ interface/src/components/GuessBoard.vue
@@ -8,14 +8,19 @@
       <div v-if="i == 1" style="flex: 0.5;">
       </div>
 
-      <button v-if="i == 2" @click="sendGues" style="border-radius: 4px; height: 58px; margin: 0 6px 0 0; flex: 1.5;" class="flex items-center content-center justify-center text-2xl font-bold text-center capitalize bg-gray-500 cursor-pointer norm rounded-md keyText">&#128797;</button>
+      <button v-if="i == 2" @click="sendGues" style="border-radius: 4px; height: 58px; margin: 0 6px 0 0; flex: 1.5;" class="flex items-center content-center justify-center text-sm font-bold text-center uppercase bg-gray-500 cursor-pointer norm rounded-md keyText">Enter</button>
 
       <button @click="appendLetter(letter)" v-for="letter in row" :key="letter" style="border-radius: 4px; height: 58px; margin: 0 6px 0 0;" class="flex items-center content-center justify-center flex-1 font-bold text-center capitalize bg-gray-500 cursor-pointer rounded-md keyText" :class="letterStyle(letter)">
         {{ letter }}
       </button>
 
       <button @click="popLetter" v-if="i == 2" style="border-radius: 4px; height: 58px; margin: 0 6px 0 0; flex: 1.5;" class="flex items-center content-center justify-center text-3xl font-bold text-center capitalize bg-gray-500 cursor-pointer norm rounded-md keyText">
-        <span class="text-2xl">&#128848;</span>
+        <span class="text-2xl">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
+</svg>
+
+        </span>
         <!--
         <svg class="del-key" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
           <path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
